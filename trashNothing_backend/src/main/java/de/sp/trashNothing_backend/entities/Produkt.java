3103715e3df018c92e2368580714db0af69aa1c4
lalagum <maksmadeng@gmail.com>
2024-10-29 13:37:12 +0100

    add Entity first version

---
+++ trashNothing_backend/src/main/java/de/sp/trashNothing_backend/entities/Produkt.java
@@ -0,0 +1,68 @@
+package de.sp.trashNothing_backend.entities;
+
+import de.sp.trashNothing_backend.entities.enumClass.Kategorie;
+import de.sp.trashNothing_backend.entities.enumClass.Zustand;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Produkt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank(message = "Titel darf nicht leer sein")
+    private String titel;
+
+    @NotBlank(message = "Beschreibung darf nicht leer sein")
+    @Size(max = 1000, message = "Die Beschreibung darf maximal 1000 Zeichen lang sein")
+    private String beschreibung;
+
+    @Min(value = 0, message = "Die Anzahl muss mindestens 0 sein")
+    private int anzahl;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Der Preis muss größer als 0 sein")
+    private BigDecimal preis;
+
+    @NotNull(message = "Zustand darf nicht leer sein")
+    @Enumerated(EnumType.STRING)
+    private Zustand zustand;
+
+    @NotBlank(message = "Marke darf nicht leer sein")
+    private String marke;
+
+    private boolean lieferung;
+
+    @Column(length = 1300)
+    private String imgUrl;
+
+    @Column(length = 1300)
+    private String deleteUrl;
+
+    @NotNull(message = "Kategorie darf nicht leer sein")
+    @Enumerated(EnumType.STRING)
+    private Kategorie kategorie;
+
+    @ManyToOne
+    @JoinColumn(name = "benutzer_id")
+    private Benutzer benutzer;
+
+    @OneToMany(mappedBy = "produkt")
+    private List<WunschSet_Produkt> wunschSetProdukte;
+
+    @OneToMany(mappedBy = "produkt")
+    private List<GekauftSet_Produkt> gekauftSetProdukte;
+
+    public Produkt() {
+    }
+}
