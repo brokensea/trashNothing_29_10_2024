7338525c11a2291e78356b7326966ce71dfa7819
flyooy <flyooy@gmail.com>
2024-10-29 13:19:32 +0100

    Components and pages created. In Main.jsx created DOM for navigation.

--- trashNothing_frond/src/main.jsx
+++ trashNothing_frond/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  // {
+  //   path: "/login",
+  //   element: <LoginPage />
+  // },
+  // {
+  //   path: "/registration",
+  //   element: <RegistrationPage />
+  // },
+  
+  // {
+  //   path: "/user",
+  //   element: <ProtectedRoute element={<UserPage />} />
+  // }
+  
+  
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
