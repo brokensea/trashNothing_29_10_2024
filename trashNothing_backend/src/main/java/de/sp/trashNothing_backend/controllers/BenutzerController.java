aa9b895d13d2bd33d4d1bfb560d45ad44ffcced9
marcusslomka <marcus.slomka@googlemail.com>
2024-10-30 14:32:31 +0100

    ListeVerkaufter Artikel anzeigen backend funktioniert

--- trashNothing_backend/src/main/java/de/sp/trashNothing_backend/controllers/BenutzerController.java
+++ trashNothing_backend/src/main/java/de/sp/trashNothing_backend/controllers/BenutzerController.java
@@ -3,7 +3,12 @@ package de.sp.trashNothing_backend.controllers;
 import de.sp.trashNothing_backend.dtos.request.GetAllSoldProductsRequestDto;
 import de.sp.trashNothing_backend.dtos.response.GetAllSoldProductsResponseDto;
 import de.sp.trashNothing_backend.services.BenutzerService;
+import jakarta.validation.Valid;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +22,8 @@ public class BenutzerController {
     }
 
     @GetMapping("/soldItems")
-
-    public GetAllSoldProductsResponseDto GetListeVerkaufterProdukte(GetAllSoldProductsRequestDto dto){
-        return benutzerService.GetListeverkaufterProdukte(dto.benutzerId());
+    public ResponseEntity<GetAllSoldProductsResponseDto> GetListeVerkaufterProdukte(@Valid @RequestBody GetAllSoldProductsRequestDto dto){
+        return ResponseEntity.ok(benutzerService.GetListeverkaufterProdukte(dto.benutzerId()));
 
     }
 }
