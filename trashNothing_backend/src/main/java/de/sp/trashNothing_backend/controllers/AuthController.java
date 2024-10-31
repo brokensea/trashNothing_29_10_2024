30512b08dd3b01d3ec6471f0aea0d99adbdc7f67
lalagum <maksmadeng@gmail.com>
2024-10-31 10:16:46 +0100

    add BenutzerId bei Authentification response

--- trashNothing_backend/src/main/java/de/sp/trashNothing_backend/controllers/AuthController.java
+++ trashNothing_backend/src/main/java/de/sp/trashNothing_backend/controllers/AuthController.java
@@ -2,7 +2,10 @@ package de.sp.trashNothing_backend.controllers;
 
 import de.sp.trashNothing_backend.dtos.request.AuthRequestDto;
 import de.sp.trashNothing_backend.dtos.response.AuthResponseDto;
+import de.sp.trashNothing_backend.dtos.response.TokenResponseDto;
 import de.sp.trashNothing_backend.services.AuthentificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +23,17 @@ public class AuthController {
     }
 
 
+    /* @PostMapping("/signin")
+     public String signin(Authentication authentication) {
+         System.out.println(authentication.getAuthorities().toString());
+         return authentificationService.token(authentication);
+     }*/
     @PostMapping("/signin")
-    public String signin(Authentication authentication) {
-        System.out.println(authentication.getAuthorities().toString());
-        return authentificationService.token(authentication);
+    public ResponseEntity<TokenResponseDto> signin(Authentication authentication) {
+        TokenResponseDto responseDto = authentificationService.token(authentication);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
+
 
     @PostMapping("/signup")
     public AuthResponseDto signup(@RequestBody AuthRequestDto dto) {
